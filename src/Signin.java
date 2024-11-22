64b1b03185fc9e0968176b050a7c7c17904623f3
ramaraovallabhuni <144024341+ramaraovallabhuni@users.noreply.github.com>
2024-11-22 10:56:53 +0530

    commit

--- src/Signin.java
+++ src/Signin.java
@@ -1,9 +1,51 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Signin {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Login Page");
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
 
-	}
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setBounds(10, 20, 80, 25);
+        frame.add(usernameLabel);
 
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(100, 20, 165, 25);
+        frame.add(usernameField);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        frame.add(passwordLabel);
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(100, 50, 165, 25);
+        frame.add(passwordField);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(100, 80, 100, 25);
+        frame.add(loginButton);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                if (username.equals("admin") && password.equals("password")) {
+                    JOptionPane.showMessageDialog(frame, "Login successful!");
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Invalid username or password.");
+                }
+            }
+        });
+
+        frame.setVisible(true);
+    }
 }
+
